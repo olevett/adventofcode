b7b3d1bf6ed6a48ca6012212cc1d939f82e2b331
Olly Levett <oliver.levett@softwire.com>
2015-12-08 17:26:18 +0000

    Really bodgy way to do day 8 part 1

---
+++ AdventOfCode.Tests/Day8Tests.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode.Helpers;
+using AdventOfCode.Tests.Helpers;
+using System.Linq;
+using Xunit;
+
+namespace AdventOfCode.Tests
+{
+    public class Day8Tests
+    {
+        [Theory]
+        [InlineData(@"""""", 0)]
+        [InlineData(@"""abc""", 3)]
+        [InlineData(@"""aaa\""aaa""", 7)]
+        [InlineData(@"""\x27""", 1)]
+        [InlineData(@"""\\""", 1)]
+        public void CountInMemoryCharacters(string input, int expected)
+        {
+            var day8 = new Day8();
+            var actual = day8.CountInMemoryCharacters(input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CountInMemoryCharactersFromFile()
+        {
+            var inputs = LoadFromResource.Load("AdventOfCode.Tests.TestData.Day8.txt");
+            var line1 = inputs.SplitOnNewLines().First();
+
+            var day8 = new Day8();
+            var actual = day8.CountInMemoryCharacters(line1);
+
+            Assert.Equal(7, actual);
+        }
+
+        [Theory]
+        [InlineData(@"""""", 2)]
+        [InlineData(@"""abc""", 5)]
+        [InlineData("\"aaa\\\"aaa\"", 10)]
+        [InlineData(@"""\x27""", 6)]
+        [InlineData(@"\\", 2)]
+        [InlineData(@"\""", 2)]
+        public void CountCodeCharacters(string input, int expected)
+        {
+            var day8 = new Day8();
+            var actual = day8.CountCodeCharacters(input);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CountCodeCharactersFromFile()
+        {
+            var inputs = LoadFromResource.Load("AdventOfCode.Tests.TestData.Day8.txt");
+            var line1 = inputs.SplitOnNewLines().First();
+
+            var day8 = new Day8();
+            var actual = day8.CountCodeCharacters(line1);
+
+            Assert.Equal(9, actual);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var inputs = LoadFromResource.Load("AdventOfCode.Tests.TestData.Day8.txt");
+            var day8 = new Day8();
+            var actual = day8.CharacterDifference(inputs);
+            Assert.Equal(1350, actual);
+        }
+    }
+}
---
+++ AdventOfCode/Day8.cs
@@ -0,0 +1,50 @@
+﻿using AdventOfCode.Helpers;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    public class Day8
+    {
+        private Regex hexRegex = new Regex("([\\][x][0-9a-f][0-9a-f])");
+
+        public int CountInMemoryCharacters(string input)
+        {
+            input = input.Replace("\\\"", "A");            
+            
+            var count = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '\\')
+                {
+                    if (input[i + 1] == 'x')
+                    {
+                        i += 3;
+                    }
+                    else if (input[i + 1] == '\\')
+                    {
+                        i += 1;
+                    }
+                    else if (input[i + 1] == '\"')
+                    {
+                        i += 1;
+                    }
+                }
+                count++;
+            }
+            return count-2;
+        }
+
+        public int CountCodeCharacters(string input)
+        {
+            return input.Length;
+        }
+
+        public int CharacterDifference(string input)
+        {
+            var totalInMemory = input.SplitOnNewLines().Select(CountInMemoryCharacters).Sum();
+            var totalCode = input.SplitOnNewLines().Select(CountCodeCharacters).Sum(); 
+            return totalCode - totalInMemory;
+        }
+    }
+}
